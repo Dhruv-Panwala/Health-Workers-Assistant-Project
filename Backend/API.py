3b2853d037427cdc10ea4782a31e9a2baaf9bfc9
Dhruv-Panwala <134924851+Dhruv-Panwala@users.noreply.github.com>
2026-02-04 23:41:52 +0000

    Add new allowed origin for CORS

--- Backend/API.py
+++ Backend/API.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://health-worker-assistant.netlify.app",
+        "https://health-workers-assistant.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
